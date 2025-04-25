ab6765525da75d447655c8a507cb18d7c4b653e0
Desarrollo Valle Conecta <informatica.aps.vallenar@gmail.com>
2025-04-25 17:06:06 -0400

    Pruebas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('MARKETPLACE VALLENAR') }}
+                        {{ __('MARKETPLACE_VALLENAR') }}
                     </x-nav-link>
                 </div>
             </div>
